d8e34032a8abaf0e1df7e923e5e4e364636d9c2c
Manoj Kumar Konda <manojkonda@Manojs-MacBook-Pro.local>
2026-03-27 10:44:16 -0500

    Fix: support multi-platform deployment (Vercel, Netlify, GH-Pages)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-client-harika-paamu/',
+  base: process.env.VITE_DEPLOY === 'gh-pages' ? '/portfolio-client-harika-paamu/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
